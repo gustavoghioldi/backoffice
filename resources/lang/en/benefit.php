5fd01e930bb1bce58f6053c18f119662d36af592
ghi <gustavoghioldi@gmail.com>
2016-12-10 10:50:48 -0300

    [master] -- side bar incomplete

---
+++ resources/lang/en/benefit.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    'home' => 'Home',
+    'partners'     => 'Partners',
+    'clients' => 'Clientes',
+    'members' => 'Miembros',
+    'benefits' => 'Beneficios',
+    'communities' => 'Comunidades', 
+];
